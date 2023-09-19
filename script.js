69b2edc529998ece778faf58807b06ee42c4e02a
José Pires <103378551+jose-pires-neto@users.noreply.github.com>
2023-09-19 11:58:29 -0300

    (Feat): Comentarios no codigo javascript, remoção do botão de adicionar equações

--- script.js
+++ script.js
@@ -1,26 +1,16 @@
-function addEquation() {
-    const equationsDiv = document.getElementById("equations");
-    const equationDiv = document.createElement("div");
-    equationDiv.className = "equation";
-    const index = equationsDiv.children.length + 1;
-    equationDiv.innerHTML = `
-      <label for="a${index}1">a${index}1:</label>
-      <input type="number" class="a" name="a${index}1" value="0">
-      <label for="a${index}2">a${index}2:</label>
-      <input type="number" class="a" name="a${index}2" value="0">
-      <label for="b${index}">b${index}:</label>
-      <input type="number" class="b" name="b${index}" value="0">
-    `;
-    equationsDiv.appendChild(equationDiv);
-  }
-
+// Esta função implementa o método de Gauss-Seidel para resolver um sistema de equações lineares
   function gaussSeidel() {
+    
+    // Aqui ele obtem os elementos HTML que representam as equações
     const equations = document.getElementsByClassName("equation");
     const a = [];
     const b = [];
     const x = [];
+
+    // Iterar sobre cada equação e extrair os valores dos inputs
     for (let i = 0; i < equations.length; i++) {
       const aRow = [];
+      // Obter os coeficientes a[i][0] e a[i][1] da equação i
       const a1 = equations[i].querySelector(`input[name="a${i+1}1"]`);
       const a2 = equations[i].querySelector(`input[name="a${i+1}2"]`);
       if (a1 !== null && a2 !== null && !isNaN(parseFloat(a1.value)) && !isNaN(parseFloat(a2.value))) {
@@ -28,38 +18,52 @@ function addEquation() {
         aRow.push(parseFloat(a2.value));
         a.push(aRow);
       }
+      // Obter o termo independente b[i] da equação i
       const bInput = equations[i].querySelector(`input[name="b${i+1}"]`);
       if (bInput !== null && !isNaN(parseFloat(bInput.value))) {
         b.push(parseFloat(bInput.value));
       }
+      // Obter o valor inicial x[i] da solução
       const xInput = document.getElementById(`x${i+1}`);
       if (xInput !== null && !isNaN(parseFloat(xInput.value))) {
         x.push(parseFloat(xInput.value));
       }
     }
+
+    // Definir o número máximo de iterações e a tolerância para o critério de parada
     const maxIterations = 100;
     const tolerance = 0.0001;
+
+    // Inicializar o contador de iterações e o erro máximo
     let iteration = 0;
     let error = 1;
+
+    // Repetir o processo até que o número máximo de iterações seja atingido ou o erro máximo seja menor que a tolerância
     while (iteration < maxIterations && error > tolerance) {
       for (let i = 0; i < equations.length; i++) {
         let sum = 0;
         for (let j = 0; j < equations.length; j++) {
           if (j !== i) {
+            // Calcular a soma dos produtos dos coeficientes e as soluções, exceto para o termo diagonal
             sum += a[i][j] * x[j];
           }
         }
+        // Calcular o novo valor de x[i] usando a fórmula de Gauss-Seidel
         const xNew = (b[i] - sum) / a[i][i];
+        // Calcular o erro absoluto entre o novo e o antigo valor de x[i]
         error = Math.abs(xNew - x[i]);
+        // Atualizar o valor de x[i] com o novo valor
         x[i] = xNew;
       }
+      // Incrementa o contador de iterações
       iteration++;
     }
+    // Iterar sobre cada equação e mostrar o valor final de x[i] no elemento HTML correspondente
     for (let i = 0; i < equations.length; i++) {
       const xInput = document.getElementById(`x${i+1}`);
       if (xInput !== null && !isNaN(parseFloat(x[i]))) {
+        // Arredondar o valor de x[i] para quatro casas decimais e atribuí-lo ao input
         xInput.value = x[i].toFixed(4);
       }
     }
   }
-
